8dcddef1fb3ee2a25f711aa8e8baf989f90e0030
TalentFly <1483118244@qq.com>
2019-12-24 23:01:27 +0800

    最后一个单词长度

--- src/cn/com/nb/leetcode/ClimbStairs.java
+++ src/cn/com/nb/leetcode/ClimbStairs.java
@@ -1,9 +1,15 @@
 package cn.com.nb.leetcode;
 
 /**
- * @description:
+ * @description: 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * <p>
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
  * @author: GOD
  * @date: 2019/12/18 下午3:42
  */
 public class ClimbStairs {
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/cn/com/nb/leetcode/LengthOfLastWord.java
+++ src/cn/com/nb/leetcode/LengthOfLastWord.java
@@ -1,9 +1,24 @@
 package cn.com.nb.leetcode;
 
 /**
- * @description:
+ * @description: 给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+ * <p>
+ * 如果不存在最后一个单词，请返回 0 。
+ * <p>
+ * 说明：一个单词是指由字母组成，但不包含任何空格的字符串。
  * @author: GOD
  * @date: 2019/12/24 下午10:43
  */
 public class LengthOfLastWord {
+
+    public static void main(String[] args) {
+        String word = "Hello World";
+        System.out.println(lengthOfLastWord(word));
+    }
+
+    private static int lengthOfLastWord(String s) {
+        if (s == null || "".equals(s.trim())) return 0;
+        String[] sArray = s.split(" ");
+        return sArray[sArray.length - 1].length();
+    }
 }
