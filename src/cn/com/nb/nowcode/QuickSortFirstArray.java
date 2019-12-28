0b7ffca09a8e29c974f595620b9e804fee89c6b2
TalentFly <1483118244@qq.com>
2019-12-28 22:30:34 +0800

    牛课网快速排序第一次结果顺序

--- src/cn/com/nb/nowcode/QuickSortFirstArray.java
+++ src/cn/com/nb/nowcode/QuickSortFirstArray.java
@@ -6,4 +6,39 @@ package cn.com.nb.nowcode;
  * @date: 2019/12/28 上午11:06
  */
 public class QuickSortFirstArray {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{12, 15, 1, 18, 2, 35, 30, 11};
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] array, int l, int r) {
+        if (l < r) {
+            int middel = partition(array, l, r);
+            quickSort(array, l, middel - 1);
+            quickSort(array, middel + 1, r);
+        }
+
+    }
+
+    private static int partition(int[] array, int l, int r) {
+        int x = array[l];
+        while (l < r) {
+            while (l < r && array[r] > x) {
+                r--;
+            }
+            array[l] = array[r];
+            while (l < r && array[l] <= x) {
+                l++;
+            }
+            array[r] = array[l];
+        }
+        array[l] = x;
+
+        for (int element : array) {
+            System.out.print(element + "\t");
+        }
+        System.exit(0);
+        return l;
+    }
 }
