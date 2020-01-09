5f27736306a5e57a6e22336a93d62ed74c9ecb55
TalentFly <1483118244@qq.com>
2020-01-09 23:11:52 +0800

    杨辉三角二

--- src/cn/com/nb/leetcode/YangHui.java
+++ src/cn/com/nb/leetcode/YangHui.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class YangHui {
 
     public static void main(String[] args) {
-        System.out.println(generate(5));
+        System.out.println(getRow(3));
     }
 
     private static List<List<Integer>> generate(int numRows) {
@@ -30,5 +30,23 @@ public class YangHui {
             result.add(element);
         }
         return result;
+    }
+
+    private static List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> result = new ArrayList<>();
+        int[][] array = new int[rowIndex + 1][rowIndex + 1];
+        for (int i = 0; i <= rowIndex; i++) {
+            List<Integer> r = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    array[i][j] = 1;
+                } else {
+                    array[i][j] = array[i - 1][j - 1] + array[i - 1][j];
+                }
+                r.add(array[i][j]);
+            }
+            result.add(r);
+        }
+        return result.get(rowIndex);
     }
 }
